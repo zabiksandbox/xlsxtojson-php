0d0ac636e6d040eb8df502149bac0aa1dd98bf18
zabik <zabik@bnwall.net>
2022-02-12 04:00:54 +0300

    Config example (#1)

--- src/XlsxtojsonSettings.php
+++ src/XlsxtojsonSettings.php
@@ -13,17 +13,15 @@ class XlsxtojsonSettings
 	public function __construct(){
 		$dotenv = new \Symfony\Component\Dotenv\Dotenv();
 
-		if (!file_exists(__DIR__.'/../../../../.env')) {$fh = fopen(__DIR__.'/../../../../.env', 'w') or die("Can't create .env file");}
-		if (!file_exists(__DIR__.'/../../../../.env')) {$fh = fopen(__DIR__.'/../../../../.env.dev', 'w') or die("Can't create env.dev file");}
+		$rootDir = __DIR__.'/../../../../';
 
-        $dotenv->load(__DIR__.'/../../../../.env', __DIR__.'/../../../../.env');
+        $dotenv->load($rootDir.'.env');
 
         $this->TIMEOUT_SEC 				= isset($_ENV['XLSXTOJSON_TIMEOUT_SEC']) ? $_ENV['XLSXTOJSON_TIMEOUT_SEC'] : 30;
 	    $this->CACERT_PEM 				= isset($_ENV['XLSXTOJSON_CACERT_PEM']) ? $_ENV['XLSXTOJSON_CACERT_PEM'] : false;
 	    $this->CACERT_KEY 				= isset($_ENV['XLSXTOJSON_CACERT_KEY']) ? $_ENV['XLSXTOJSON_CACERT_KEY'] : false;
 	    $this->CACERT_KEY_PASSPHRASE 	= isset($_ENV['XLSXTOJSON_CACERT_KEY_PASSPHRASE']) ? $_ENV['XLSXTOJSON_CACERT_KEY_PASSPHRASE'] : '';
 	    $this->API_KEY 					= isset($_ENV['XLSXTOJSON_APIKEY']) ? $_ENV['XLSXTOJSON_APIKEY'] : '';
-        
         
 	}
     
